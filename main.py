c4c0707f3f425ffcfa17ac4dfb497c67f73494ac
Muki <mramic@student.tugraz.at>
2024-04-28 13:11:23 +0200

    structure

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import os
+import logging
+from pathlib import Path
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logging.basicConfig(level=logging.INFO, format='[%(acstime)s]: %(message)s')
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+project_name = 'cls'
